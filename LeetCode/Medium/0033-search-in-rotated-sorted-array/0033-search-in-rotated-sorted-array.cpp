dea026f0bcd706152537b758e413e8b31edc0f0c
Priyank Shah <138972731+directionlessvector@users.noreply.github.com>
2026-01-16 17:17:45 +0530

    0033-search-in-rotated-sorted-array Medium

---
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& a, int t) {
+        int l = 0, h = a.size() - 1;
+
+        while (l <= h) {
+            int mid = l + ((h - l) >> 1);
+
+            if (a[mid] == t)
+                return mid;
+
+            if (a[l] <= a[mid]) {
+                if (a[l] <= t && t < a[mid])
+                    h = mid - 1;
+                else
+                    l = mid + 1;
+            }
+
+            else {
+                if (a[mid] < t && t <= a[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
