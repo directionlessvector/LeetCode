7348eb9954952b5c18a9dcf1123d5761b2f80ebc
Priyank Shah <138972731+directionlessvector@users.noreply.github.com>
2026-01-16 19:58:51 +0530

    0081-search-in-rotated-sorted-array-ii Medium

--- LeetCode/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ LeetCode/Medium/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool search(vector<int>& a, int t) {
-        int n = a.size();
-        int l = 0, h = n - 1;
+        int l = 0, h = a.size() - 1;
 
-        while (l < h) {
+        while (l <= h) {
             int mid = l + ((h - l) >> 1);
 
-            if (a[mid] == t)
-                return true;
-            
-            while (a[mid] == a[l]) l++;
-            while (a[mid] == a[h]) h--;
+            if (a[mid] == t) return true;
 
-            if (a[mid] <= a[l]) {
-                if (a[l] <= t && t < a[mid]) 
+            if (a[l] == a[mid] && a[mid] == a[h]) {
+                l++;
+                h--;
+            }
+            
+            else if (a[l] <= a[mid]) {
+                if (a[l] <= t && t < a[mid])
                     h = mid - 1;
-                else 
+                else
                     l = mid + 1;
             }
 
             else {
-                if (a[mid] < t && t < a[h])
+                if (a[mid] < t && t <= a[h])
                     l = mid + 1;
-                else 
+                else
                     h = mid - 1;
             }
         }
-
-        return false; 
+        return false;
     }
 };
