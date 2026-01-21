fb7f5832ee0d920456bf3910228d3fb4fb6d5400
Priyank Shah <138972731+directionlessvector@users.noreply.github.com>
2026-01-21 08:34:37 +0530

    3315-construct-the-minimum-bitwise-array-ii Medium

---
+++ LeetCode/Medium/3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for (int &x : nums) {
+            if (x & 1) {
+                int t = ((x + 1) & ~x) >> 1;
+                ans.push_back(x & ~t);
+            }
+            else 
+                ans.push_back(-1);
+        }
+        return ans;
+    }
+};
